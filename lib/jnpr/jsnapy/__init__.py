a0e7940cc7f20575151bc42bdf3369f6f6215885
nitin kumar <nitin.nitp@gmail.com>
2016-03-14 16:57:46 +0530

    get_path should get called once

--- lib/jnpr/jsnapy/__init__.py
+++ lib/jnpr/jsnapy/__init__.py
@@ -2,13 +2,19 @@ from .version import __version__
 import ConfigParser
 import os
 
+class Singleton(object):
+  _instance = None
+  def __new__(cls, section, value, *args, **kwargs):
+    if not isinstance(cls._instance, str):
+        config = ConfigParser.ConfigParser({'config_file_path': '/etc/jsnapy', 'snapshot_path': '/etc/jsnapy/snapshots',
+                                        'test_file_path': '/etc/jsnapy/testfiles', 'log_file_path': '/etc/logs/jsnapy'})
+        config.read(os.path.join('/etc', 'jsnapy', 'jsnapy.cfg'))
+        path = config.get(section, value)
+
+        cls._instance = path
+    return cls._instance
 
 def get_path(section, value):
-    config = ConfigParser.ConfigParser({'config_file_path': '/etc/jsnapy', 'snapshot_path': '/etc/jsnapy/snapshots',
-                                        'test_file_path': '/etc/jsnapy/testfiles', 'log_file_path': '/etc/logs/jsnapy'})
-    config.read(os.path.join('/etc', 'jsnapy', 'jsnapy.cfg'))
-
-    path = config.get(section, value)
-    return path
+    return Singleton(section, value)
 
 from jnpr.jsnapy.jsnapy import SnapAdmin
