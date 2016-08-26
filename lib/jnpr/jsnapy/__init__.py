4a480c09e267ad7cab571346ee78ad8db42ec074
Ishaan Kumar <ishaank@juniper.net>
2016-08-26 15:02:51 +0530

    Added exception raise to init.py

--- lib/jnpr/jsnapy/__init__.py
+++ lib/jnpr/jsnapy/__init__.py
@@ -12,7 +12,10 @@ def get_path(section, value):
     # config = ConfigParser.ConfigParser({'config_file_path': '/usr/local/share/', 'snapshot_path': '/usr/local/share/snapshots',
     #                                     'test_file_path': '/usr/local/share/testfiles', 'log_file_path': '/var/log/jsnapy'})
     config = ConfigParser.ConfigParser()
-    config.read(os.path.join('/etc', 'jsnapy', 'jsnapy.cfg'))
+    config_location = os.path.join('/etc', 'jsnapy', 'jsnapy.cfg')
+    if not os.path.isfile(config_location):
+        raise Exception('Config file not found at %s '%config_location)
+    config.read(config_location)
     # config.read(os.path.join('/usr/local/share', 'jsnapy', 'jsnapy.cfg'))
     path = config.get(section, value)
     return path
