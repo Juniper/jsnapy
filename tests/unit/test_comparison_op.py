c2b3c84509977c26f64bca1f75327c813731b562
Ishaan Kumar <ishaank@juniper.net>
2016-09-08 18:26:05 +0530

    added tests for list-not-more

--- tests/unit/test_comparison_op.py
+++ tests/unit/test_comparison_op.py
@@ -199,7 +199,96 @@ class TestComparisonOperator(unittest.TestCase):
             "snap_3")
         self.assertEqual(oper.no_passed, 2)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_list_not_more_ignore_null_fail(self, mock_path):
+        self.chk = True
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_list-not-more_ignore-null_fail.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_no-diff_pre",
+            self.action,
+            "snap_3")
+        self.assertEqual(oper.no_passed, 0)
+        self.assertEqual(oper.no_failed, 2)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_list_not_more_ignore_null_fail_1(self, mock_path):
+        self.chk = True
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_list-not-more_ignore-null_fail_1.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_no-diff_pre",
+            self.action,
+            "snap_3")
+        self.assertEqual(oper.no_passed, 2)#the name is kinda misleading but in line with the convention    
+        self.assertEqual(oper.no_failed, 0)
+ 
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_list_not_more_ignore_null_skip(self, mock_path):
+        self.chk = True
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_list-not-more_ignore-null_skip.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_no-diff_pre",
+            self.action,
+            "snap_3")
+        self.assertEqual(oper.no_passed, 0)
+        self.assertEqual(oper.no_failed, 0)
 
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_list_not_more_ignore_null_skip_1(self, mock_path):
+        self.chk = True
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_list-not-more_ignore-null_skip_1.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_no-diff_pre",
+            self.action,
+            "snap_3")
+        self.assertEqual(oper.no_passed, 2)
+        self.assertEqual(oper.no_failed, 0)
+    
+    
     @patch('jnpr.jsnapy.check.get_path')
     def test_delta(self, mock_path):
         self.chk = True
