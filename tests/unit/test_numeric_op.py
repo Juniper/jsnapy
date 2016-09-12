71d3a1fd23a6183ce7ed54036635882bb85f01fd
Ishaan Kumar <ishaank@juniper.net>
2016-09-12 18:09:32 +0530

    test for in-range,not-range, is-lt - ignore_pass

--- tests/unit/test_numeric_op.py
+++ tests/unit/test_numeric_op.py
@@ -138,6 +138,26 @@ class TestNumericOperators(unittest.TestCase):
             "snap_in-range_pre")
         self.assertEqual(oper.no_passed, 0)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_in_range_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_in-range_ignore-null_pass.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_in-range_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 1)
 
     @patch('jnpr.jsnapy.check.get_path')
     def test_not_range_fail(self, mock_path):
@@ -238,6 +258,26 @@ class TestNumericOperators(unittest.TestCase):
             "snap_not-range_pre")
         self.assertEqual(oper.no_passed, 0)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_not_range_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_not-range_ignore-null_pass.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_in-range_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 1)
 
     @patch('jnpr.jsnapy.check.get_path')
     def test_not_range_pass(self, mock_path):
@@ -378,6 +418,26 @@ class TestNumericOperators(unittest.TestCase):
             "snap_is-lt_fail_pre")
         self.assertEqual(oper.no_passed, 0)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_is_lt_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_is-lt_ignore-null_pass.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_in-range_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 1)
 
     @patch('jnpr.jsnapy.check.get_path')
     def test_is_gt(self, mock_path):
