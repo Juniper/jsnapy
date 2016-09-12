1ea01cccf32be6595171e75a3b4c708aecb2fb7d
Ishaan Kumar <ishaank@juniper.net>
2016-09-12 20:28:58 +0530

    added remaining tests

--- tests/unit/test_string_op.py
+++ tests/unit/test_string_op.py
@@ -137,6 +137,26 @@ class TestStringOperators(unittest.TestCase):
             "snap_contains_pre")
         self.assertEqual(oper.no_passed, 0)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_contains_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_contains_ignore-null_pass.yml')
+        config_file = open(conf_file, 'r')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_contains_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 1)
 
     @patch('jnpr.jsnapy.check.get_path')
     def test_is_in(self, mock_path):
@@ -257,9 +277,29 @@ class TestStringOperators(unittest.TestCase):
             "snap_is-in_fail_pre")
         self.assertEqual(oper.no_passed, 0)
         self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_is_in_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_is-in_ignore-null_pass.yml')
+        config_file = open(conf_file, 'r')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_is-in_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 2)
 
     @patch('jnpr.jsnapy.check.get_path')
-    def test_not_in(self, mock_path):
+    def test_not_in_pass(self, mock_path):
         self.chk = False
         comp = Comparator()
         conf_file = os.path.join(os.path.dirname(__file__),
@@ -275,8 +315,8 @@ class TestStringOperators(unittest.TestCase):
             self.db,
             self.snap_del,
             "snap_not-in_pre")
-        self.assertEqual(oper.no_passed, 0)
-        self.assertEqual(oper.no_failed, 1)
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 0)
 
     @patch('jnpr.jsnapy.check.get_path')
     def test_not_in_ignore_null_fail(self, mock_path):
@@ -355,11 +395,31 @@ class TestStringOperators(unittest.TestCase):
             self.db,
             self.snap_del,
             "snap_not-in_pre")
-        self.assertEqual(oper.no_passed, 0)
+        self.assertEqual(oper.no_passed, 1)
+        self.assertEqual(oper.no_failed, 0)
+    
+    @patch('jnpr.jsnapy.check.get_path')
+    def test_not_in_ignore_null_pass(self, mock_path):
+        self.chk = False
+        comp = Comparator()
+        conf_file = os.path.join(os.path.dirname(__file__),
+                                 'configs', 'main_not-in_ignore-null_pass.yml')
+        mock_path.return_value = os.path.join(os.path.dirname(__file__), 'configs')
+        config_file = open(conf_file, 'r')
+        main_file = yaml.load(config_file)
+        oper = comp.generate_test_files(
+            main_file,
+            self.hostname,
+            self.chk,
+            self.diff,
+            self.db,
+            self.snap_del,
+            "snap_not-in_pre_ignore_null")
+        self.assertEqual(oper.no_passed, 1)
         self.assertEqual(oper.no_failed, 0)
 
     @patch('jnpr.jsnapy.check.get_path')
-    def test_not_in_pass(self, mock_path):
+    def test_not_in(self, mock_path):
         self.chk = False
         comp = Comparator()
         conf_file = os.path.join(os.path.dirname(__file__),
@@ -375,8 +435,8 @@ class TestStringOperators(unittest.TestCase):
             self.db,
             self.snap_del,
             "snap_not-in_fail_pre")
-        self.assertEqual(oper.no_passed, 1)
-        self.assertEqual(oper.no_failed, 0)
+        self.assertEqual(oper.no_passed, 0)
+        self.assertEqual(oper.no_failed, 1)
 
 with patch('logging.Logger') as mock_logger:
     if __name__ == "__main__":
