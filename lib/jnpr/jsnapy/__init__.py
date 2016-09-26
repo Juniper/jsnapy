4c770d843ea4d0943200fdfae424de04764743fc
Ishaan Kumar <ishaank@juniper.net>
2016-09-26 16:54:57 +0530

    changed get_path to honour cfg lookup heirarchy

--- lib/jnpr/jsnapy/__init__.py
+++ lib/jnpr/jsnapy/__init__.py
@@ -12,9 +12,21 @@ def get_path(section, value):
     # config = ConfigParser.ConfigParser({'config_file_path': '/usr/local/share/', 'snapshot_path': '/usr/local/share/snapshots',
     #                                     'test_file_path': '/usr/local/share/testfiles', 'log_file_path': '/var/log/jsnapy'})
     config = ConfigParser.ConfigParser()
-    config_location = os.path.join('/etc', 'jsnapy', 'jsnapy.cfg')
-    if not os.path.isfile(config_location):
-        raise Exception('Config file not found at %s '%config_location)
+    config_location = None
+    
+    p_locations = []
+    if 'JSNAPY_HOME' in os.environ:
+        p_locations = [os.environ['JSNAPY_HOME']]   
+    p_locations.extend(['~/.jsnapy','/etc/jsnapy'])
+    
+    for loc in p_locations:
+        possible_location =  os.path.join(loc,'jsnapy.cfg')
+        if os.path.isfile(possible_location):
+            config_location = possible_location
+            break
+    
+    if config_location is None:
+        raise Exception('Config file not found at {0}'.format(repr(p_locations)))
     config.read(config_location)
     # config.read(os.path.join('/usr/local/share', 'jsnapy', 'jsnapy.cfg'))
     path = config.get(section, value)
