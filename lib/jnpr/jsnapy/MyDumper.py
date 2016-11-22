e7a9e31e5024ddf2957ea8f2b2309d05d0c09179
awasthiy <yawasthi32@gmail.com>
2016-11-22 19:28:10 +0530

    new file added

---
+++ lib/jnpr/jsnapy/MyDumper.py
@@ -0,0 +1,5 @@
+import yaml
+import sys
+class MyDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
