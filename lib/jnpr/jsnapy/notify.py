6aff4a7978a6f61bbb54a4f18abbb4a5eada3bea
Jasminderpal Singh Sidhu <jammy.sidhu43@gmail.com>
2017-05-09 14:12:39 +0530

    mail notifications now for windows

--- lib/jnpr/jsnapy/notify.py
+++ lib/jnpr/jsnapy/notify.py
@@ -13,6 +13,7 @@ import colorama
 import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import posixpath
 
 
 class Notification:
@@ -32,9 +33,11 @@ class Notification:
             colorama.Fore.BLUE +
             "Sending mail............", extra=self.log_details)
         testdetails = test_obj.test_details
-        templateLoader = jinja2.FileSystemLoader(searchpath="/")
+        drive, tail = os.path.splitdrive(__file__)
+        templateLoader = jinja2.FileSystemLoader(searchpath=[drive + "\\", "/"])
         templateEnv = jinja2.Environment(loader=templateLoader)
-        TEMPLATE_FILE = os.path.join(os.path.dirname(__file__), 'content.html')
+        TEMPLATE_FILE = posixpath.join(os.path.dirname(__file__), 'content.html')
+        TEMPLATE_FILE = posixpath.join(*TEMPLATE_FILE.split('\\'))
         template = templateEnv.get_template(TEMPLATE_FILE)
         outputText = template.render(device=hostname, name=mail_file['recipient_name'], tests=testdetails, 
                                      date=time.ctime(),
