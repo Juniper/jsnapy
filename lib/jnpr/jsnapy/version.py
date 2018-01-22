013a0b6a2cdcf5e722e8a02b160abc523a9e9f63
Nitin Kr <nitinkr@juniper.net>
2018-01-22 17:44:46 +0530

    preparing new release for 1.3.0

--- lib/jnpr/jsnapy/version.py
+++ lib/jnpr/jsnapy/version.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-# Copyright (c) 1999-2017, Juniper Networks Inc.
+# Copyright (c) 1999-2018, Juniper Networks Inc.
 #
 # All rights reserved.
 #
 
-__version__ = "1.3.0.dev0"
-DATE = "2018-Jan-15"
+__version__ = "1.3.0"
+DATE = "2018-Jan-22"
