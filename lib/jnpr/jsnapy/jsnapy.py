c816e52c960bc152b8c3675d514f7c986e946684
rahkumar651991 <rahkumar@juniper.net>
2020-09-10 21:11:54 +0530

    Rearranging code to print correct version

--- lib/jnpr/jsnapy/jsnapy.py
+++ lib/jnpr/jsnapy/jsnapy.py
@@ -1044,6 +1044,7 @@ def main():
         if js.args.version is True:
             print("JSNAPy version: %s" % version.__version__)
         else:
+            js.check_arguments()
             if js.args.verbosity:
                 js.set_verbosity(10 * js.args.verbosity)
             try:
